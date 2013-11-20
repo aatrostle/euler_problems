f69d65da905f22aa7ca5c327f86e465a1fed9284
Aaron Trostle <aaron.trostle@gmail.com>
2013-11-19 19:01:30 -0800

    update problem_2 solution

--- problem_2/problem_2.py
+++ problem_2/problem_2.py
@@ -2,20 +2,13 @@
 # whos values do not exceed four million
 # find the sum of the even-valued terms
 
-fibs = [0,1,1]
-fibssum = []
-result = 2
-val = 4000000
-while (result < val):
-  if result % 2 == 0:
-    fibssum.append(result)
-  fibs.append(result)
-  result = fibs[-1] + fibs[-2]
-
-print "fibs is %r \n and fibssum is %r" % (fibs, fibssum)
-
-result = fibs[-1] + fibs[-2]
-fibs.append(result)
-
-print "sanity check that next fibs is over 4mil: %r" % fibs
-print "\nfinal sum is %r" % sum(fibssum)
+sum_of_even_fibs = 0
+fibs = [ 0, 1 ]
+while True:
+  fib = sum(fibs)
+  if fib > 4000000:
+    break
+  if fib % 2 == 0:
+    sum_of_even_fibs += fib
+  fibs = [fibs[1], fib] 
+  print sum_of_even_fibs
